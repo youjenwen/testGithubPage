9d4af4eb17804166fd1c6311cfe8716a97d0efb4
TW-Web-Harper Wen <harper.wen@shoalter.com>
2023-04-14 11:56:44 +0800

    feat: add toggle in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import React from "react";
+import Toggle from "./components/toggle";
 
 function App() {
-
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Toggle />
+    </>
+  );
 }
 
-export default App
+export default App;
